3886852a98a820ba3fb4b220bd83d053a42b9cbf
Александр Комаров <kai___@rambler.ru>
2025-11-27 22:24:00 +0300

    исправление JS по функционалу поиска, начало внесения правок по изменению поведения ссылки Обновить, для обновления таблицы токенов без перезагрузки страницы

--- tokens_app/static/js/custom.js
+++ tokens_app/static/js/custom.js
@@ -1,54 +1,85 @@
 ﻿//поиск пользователей с формированием карточек
 $(document).ready(function () {
-      const $input = $('#search-input');
-      const $results = $('#results');
+  const $input = $('#search-input');
+  const $results = $('#results');
+  const $form = $('#tokenForm');
+  const $tableTokenBody = $('#dataTable tbody');
+  const $linkUpdate = $('#link_update');
 
-      $input.on('input', function () {
-        const query = $input.val().trim();
+  // Функция для назначения обработчиков клика на карточки
+  function assignCardClickHandlers() {
+    $('.card-clickable').off('click').on('click', function () {
+      const samAccountName = $(this).data('samaccountname');
 
-        if (query.length < 4) {
-          $results.empty();
-          return;
-        }
-
-        $.getJSON('/search', { q: query, limit: 5 }, function (data) {
-          $results.empty();
-
-          if (data.length === 0) {
-            $results.append('<p>Ничего не найдено</p>');
-            return;
-          }
-
-          data.forEach(function (item) {
-            $results.append(`
-              <div class="col">
-                <div class="card card-clickable" data-samaccountname="${item.sAMAccountName}">
-                  <div class="card-body">
-                    <h5 class="card-title">${item.cn}</h5>
-                    <p class="card-text">${item.description || 'Нет описания'}</p>
-                  </div>
-                  <div class="card-footer">${item.sAMAccountName}</div>
-                </div>
-              </div>
-            `);
-          assignClickHandlers();
-          });
-        });
-      });
+      $form.label.value = samAccountName;
+      // Визуальное выделение
+      $('.card-clickable').removeClass('text-bg-success');
+      $(this).addClass('text-bg-success');
     });
+  }
 
-function assignClickHandlers() {
-  const cards = document.querySelectorAll('.card-clickable');
-  const form = document.getElementById('tokenForm');
-  cards.forEach(card => {
-    card.addEventListener('click', function() {
-      cards.forEach(c => c.classList.remove('text-bg-success'));
-      this.classList.add('text-bg-success');
-      const samaccountname = this.getAttribute('data-samaccountname');
-      form.label.value = samaccountname;
+  $linkUpdate.on('click', function (e) {
+    e.preventDefault();
+    $.getJSON('/update_tokens', function (data) {
+      $tableTokenBody.empty();
+      if (data.length === 0) {
+        $tableBody.append('<tr><td colspan="4" class="text-center">Нет данных</td></tr>');
+        return;
+      }
+      data.forEach(function (item) {
+        const row = `
+            <tr data-model="${item.model}" data-sn="${item.serial_num}" data-sn-raw="${item.serial_num_raw}"
+            data-label="${item.label}" data-min-pin="${item.min_pin_user}">
+                <td>${item.model}</td>
+                <td>${item.serial_num_raw}</td>
+                <td>${item.serial_num}</td>
+                <td>${item.label}</td>
+                <td>${item.min_pin_user}</td>
+            </tr>`;
+        $tableBody.append(row);
+      });
+
+    }).fail(function () {
+      $tableBody.html('<tr><td colspan="4" class="text-center text-danger">Ошибка загрузки</td></tr>');
     });
   });
-}
+
+  $input.on('input', function () {
+    const query = $input.val().trim();
+
+    if (query.length < 4) {
+      $results.empty();
+      return;
+    }
+
+    $.getJSON('/search', { q: query, limit: 5 }, function (data) {
+      $results.empty();
+
+      if (data.length === 0) {
+        $results.append('<p>Ничего не найдено</p>');
+        return;
+      }
+
+      data.forEach(function (item) {
+        const cardHtml = `
+                    <div class="col">
+                      <div class="card card-clickable" data-samaccountname="${item.sAMAccountName}">
+                        <div class="card-body">
+                        <h5 class="card-title">${item.cn}</h5>
+                        <p class="card-text">${item.description || 'Нет описания'}</p>
+                        </div>
+                        <div class="card-footer">${item.sAMAccountName}</div>
+                      </div>
+                    </div>`;
+        $results.append(cardHtml);
+      });
+
+      // Назначаем обработчики после добавления карточек
+      assignCardClickHandlers();
+    });
+  });
+});
+
 
 document.addEventListener('DOMContentLoaded', function () {
   const rows = document.querySelectorAll('#dataTable tbody tr');
